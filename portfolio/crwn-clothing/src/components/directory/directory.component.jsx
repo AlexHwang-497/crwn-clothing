2a76127e44d8aba1409b132be71f5643e25cbd00
alex <alexhwang01234@gmail.com>
2021-08-04 18:59:55 -0700

    79.  withRouter - update our menu items to navigate withRouterHOC

--- portfolio/crwn-clothing/src/components/directory/directory.component.jsx
+++ portfolio/crwn-clothing/src/components/directory/directory.component.jsx
@@ -10,48 +10,56 @@ class Directory extends React.Component{
         // todo: we call in super(), so that we pull in all the things we need from our React component
         super()
         this.state={
-            sections:[{
-                title: 'hats',
-                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                id: 1,
-                linkUrl: 'shop/hats'
-            },
-            {
-                title: 'jackets',
-                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                id: 2,
-                linkUrl: 'shop/jackets'
-            },
-            {
-                title: 'sneakers',
-                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                id: 3,
-                linkUrl: 'shop/sneakers'
-            },
-            {
-                title: 'womens',
-                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                size: 'large',
-                id: 4,
-                linkUrl: 'shop/womens'
-            },
-            {
-                title: 'mens',
-                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                size: 'large',
-                id: 5,
-                linkUrl: 'shop/mens'
-            }]
+            // *this will represnt each of our menu items
+            // * the data proivded contains a title and image url
+            sections: [
+                {
+                  title: 'hats',
+                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                  id: 1,
+                  linkUrl: 'hats'
+                },
+                {
+                  title: 'jackets',
+                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                  id: 2,
+                  linkUrl: ''
+                },
+                {
+                  title: 'sneakers',
+                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                  id: 3,
+                  linkUrl: ''
+                },
+                {
+                  title: 'womens',
+                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                  size: 'large',
+                  id: 4,
+                  linkUrl: ''
+                },
+                {
+                  title: 'mens',
+                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                  size: 'large',
+                  id: 5,
+                  linkUrl: ''
+                }
+              ]
+            
         }
     }
     // * inside of our render, we want to return what we had in our home page
     // * we will run a map function and we want to apss these section objects into our menu, so that we can create menu items
         // * we are also destructing the object to get what we specifically need
+    // todo: {id, ...otherSectionProps} - otherSectionprops = the rest of our keys.  we spread them all in 
+      // todo:othersection prop =title={title} imageUrl={imageUrl} size={size}
+        
     render(){
-        return(
+        return (
             <div className='directory-menu'>
-                {this.state.sections.map(({title,imageUrl,id,size})=>(
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+            {this.state.sections.map(({id, ...otherSectionProps})=>(
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         )
--- portfolio/crwn-clothing/src/components/menu-item/menu-item.components.jsx
+++ portfolio/crwn-clothing/src/components/menu-item/menu-item.components.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './menu-item.styles.scss'
+// *this is a higher order component
+import { withRouter } from 'react-router-dom'
 
 // * four our menu item we are goin to use a functional componet because we don't really need our compnoent 
     // * to hold any state
 // *div style, this allows us to post the images from directory in the background, size
-const MenuItem = ({title,imageUrl,size}) => (
-    <div className={`${size} menu-item`}>
+// ! discuss with carlos in regard to what 'history' does
+// ! discuss with carlos in regard to what 'match' does
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    // *this allows us to now click on the buttons and take us to our specific pages
+        // *this helps with our basic navigation setup
+    <div className={`${size} menu-item`} onClick = {()=> history.push(`${match.url}${linkUrl}`)}>
         <div 
             className='background-image' 
             style={{
@@ -19,4 +25,4 @@ const MenuItem = ({title,imageUrl,size}) => (
     </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
