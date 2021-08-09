d677a5e19681bce9b88ad287694612214567aed1
alex <alexhwang01234@gmail.com>
2021-08-08 18:43:43 -0700

    123.  Add to Cart Styling

--- portfolio/crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ portfolio/crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss'
 // todo:A <span> element which is used to color a part of a text:
     // todo: example; <p>My mother has <span style="color:blue">blue</span> eyes.</p>
@@ -15,6 +18,7 @@ import './collection-item.styles.scss'
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
           </div>
+          <CustomButton inverted >Add to Cart</CustomButton>
         </div>
       );
 
--- portfolio/crwn-clothing/src/components/custom-button/custom-button.component.jsx
+++ portfolio/crwn-clothing/src/components/custom-button/custom-button.component.jsx
@@ -5,13 +5,17 @@ import './custom-buttom.styles.scss';
     // *we will destructure all the other props int this otherProps and spread into our custom button
     // *we put children inside our button tag
 
-    const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+    const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
       <button
-        className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
-        {...otherProps}
-      >
-        {children}
-      </button>
+      // * we will say if inverted is true, then add the inverted class
+        // *and then in our custom button we will add in this invertd class and hwat it will do is the opposite of what our regular styoning on our custom button is
+      className={`${inverted ? 'inverted' : ''} ${
+        isGoogleSignIn ? 'google-sign-in' : ''
+      } custom-button`}
+      {...otherProps}
+    >
+      {children}
+    </button>
     );
     
 export default CustomButton;
