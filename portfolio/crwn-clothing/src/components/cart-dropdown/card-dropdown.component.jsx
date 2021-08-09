636a1ea4ed64fc1e58062f519289927c8ce0c2fa
alex <alexhwang01234@gmail.com>
2021-08-09 09:48:44 -0700

    131.  Reselect Library - updating our app w/ reselect replaced cart selectors  with memoized selectors

--- portfolio/crwn-clothing/src/components/cart-dropdown/card-dropdown.component.jsx
+++ portfolio/crwn-clothing/src/components/cart-dropdown/card-dropdown.component.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 // *we import the custom button because we need it at the very end
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import './cart-dropdown.styles.scss';
 
 // ! discuss with carlos in regard to visualizing the destructuring of the data
@@ -20,8 +21,9 @@ const CartDropdown = ({cartItems}) => (
 );
 
 // *we are dstructing cart off of state
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+// * this function will make sure that our cart dropdown copmonnet is not getiing re rendered whenver the state changes, that's unrealted to the cart items
+const mapStateToProps = (state) => ({
+  cartItems:selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
--- portfolio/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ portfolio/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{itemCount}</span>
     </div>
   );
 
@@ -19,8 +20,18 @@ const CartIcon = ({ toggleCartHidden }) => (
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
   });
-  
+// *this creates the counter for on the cart icon aka the amount of items you are purchasing
+// todo:this is a selector function
+  // todo: we are writing code that gets a state as in the whole state object and then pulls of just a small portion/slice of the state
+  // todo: we're getting cart items and then just reducing over those cart items to get a new value
+
+
+  const mapStateToProps = (state)=> ({
+    itemCount: selectCartItemsCount(state)
+
+  })
+
   export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(CartIcon);
