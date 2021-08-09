e9eb06861bb2ade1c470555521922ac598975554
alex <alexhwang01234@gmail.com>
2021-08-09 09:05:04 -0700

    127. cart item component - create cart item compnent and connected our cart dropdown menu with cart items reducer

--- portfolio/crwn-clothing/src/components/cart-dropdown/card-dropdown.component.jsx
+++ portfolio/crwn-clothing/src/components/cart-dropdown/card-dropdown.component.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import {connect} from 'react-redux'
 // *we import the custom button because we need it at the very end
 import CustomButton from '../custom-button/custom-button.component';
-
+import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+// ! discuss with carlos in regard to visualizing the destructuring of the data
+// * we are also reusing components for the cartItems.map
+const CartDropdown = ({cartItems}) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {cartItems.map(cartItem=> (
+        
+          <CartItem key={cartItem.id} item={cartItem}/>
+        ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+// *we are dstructing cart off of state
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
