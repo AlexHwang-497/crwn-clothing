822a533203ba4bf07b470ad685ac93d35be7502d
alex <alexhwang01234@gmail.com>
2021-08-08 19:29:04 -0700

    124.  cart item reducer

--- portfolio/crwn-clothing/src/redux/cart/cart.reducer.js
+++ portfolio/crwn-clothing/src/redux/cart/cart.reducer.js
@@ -2,7 +2,10 @@ import CartActionTypes from './cart.types';
 
 const INITIAL_STATE = {
     // *we want to hide our webstie when they first come 
-    hidden: true
+    hidden: true,
+    // *we need to add or clear items into the cart
+    // * we will add items everytime the user clicks
+    cartItems:[]
   };
 
   const cartReducer = (state = INITIAL_STATE, action) => {
@@ -14,6 +17,13 @@ const INITIAL_STATE = {
             //   * we are saying whatever this boolean value is here, we want the opposite
               hidden: !state.hidden
           }
+          case CartActionTypes.ADD_ITEM:
+            // *we want to return an object w/ all of the state right as it is and spread it through
+            return {
+              ...state,
+              // *this will aloow our cart items array to be our old car items and then w/ the newest action got fired right.
+              cartItems:[...state.cartItems,action.payload]
+            }
           default:
               return state;
       }
--- portfolio/crwn-clothing/src/redux/cart/cart.types.js
+++ portfolio/crwn-clothing/src/redux/cart/cart.types.js
@@ -1,5 +1,7 @@
+//! discuss with carlos what the hell is going on here
 const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD_ITEM: 'ADD_ITEM'
   };
   
   export default CartActionTypes;
