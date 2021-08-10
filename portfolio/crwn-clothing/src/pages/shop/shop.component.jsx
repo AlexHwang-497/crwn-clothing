25990b3df633a5e50758408449d96118c5e9a2e3
alex <alexhwang01234@gmail.com>
2021-08-10 09:57:50 -0700

    145. collection overview comp - most updated 8-10-2021 at 10am

--- portfolio/crwn-clothing/src/pages/shop/shop.component.jsx
+++ portfolio/crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,26 +1,21 @@
 // *because we need to store data on our page, we will make this a class page
 import React from 'react'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview'
 
-import {selectCollections} from '../../redux/shop/shop.selectors'
-// import CollectionPage from '../collection/collection.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+
+
 
 
 
 const ShopPage =({collections}) => (
   <div className='shop-page'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverview/>
+
   </div>
 )
 
-const mapStateToProps = createStructuredSelector({
-  collections:selectCollections
-})
 
 
-export default connect (mapStateToProps)(ShopPage)
+
+export default ShopPage
