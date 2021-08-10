8d21e749f59ff22b32828417850a06820ec5e862
alex <alexhwang01234@gmail.com>
2021-08-10 11:09:26 -0700

    148.  Collection Routing and Selector - i also installed lodash.memoize as well

--- portfolio/crwn-clothing/src/pages/collection/collection.component.jsx
+++ portfolio/crwn-clothing/src/pages/collection/collection.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import CollectionItem from '../../components/collection-item/collection-item.component'
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss'
 
@@ -7,8 +11,13 @@ const CollectionPage = ({match}) =>(
     
     <div className='collection-page'>
         <h2>COLLECTION PAGE</h2>
-
     </div>
 )
 
-export default CollectionPage
+// * we have the 2 paramters here now
+    // * this is necessay because onlike other selectors, this selector needs a part of the state depnding on the URL parameter
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+  });
+
+export default connect(mapStateToProps)(CollectionPage);
--- portfolio/crwn-clothing/src/redux/shop/shop.selectors.js
+++ portfolio/crwn-clothing/src/redux/shop/shop.selectors.js
@@ -3,6 +3,14 @@ import { createSelector } from 'reselect';
 
 const selectShop = state => state.shop;
 
+const COLLECTION_ID_MAP = {
+  hats:1,
+  sneakers:2,
+  jackets:3,
+  womens:4,
+  mens:5
+}
+
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
@@ -13,8 +21,11 @@ export const selectCollectionsForPreview = createSelector(
   collections => Object.keys(collections).map(key => collections[key])
 );
 
+// * this function is involved with getting our collection id so we can go to their correct pages aka a hats page or a jackets page forex.
+  // * we are finding collection.id matching the url parameter of our collection id map
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections => collections[collectionUrlParam]
+    collections => collections.find(collection => collection.id===COLLECTION_ID_MAP[collectionUrlParam])
+    // collections => collections[collectionUrlParam]
   );
